90846b6b716c7b087cf709b12dc5387b9efa371f
unknown <fredileader@gmail.com>
2024-02-09 02:28:19 +0100

    modify the component

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import UrlContainer from './components/UrlContainer'
+import LinkResult from './components/LinkResult'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  
+  const [inputValue, setInputValue] = useState('')
 
   return (
     <>
-      <UrlContainer />
+      <div className='url-container'>
+        <div className='url-wrapper'>
+          <UrlContainer setInputValue={setInputValue}/>
+          <LinkResult inputValue={inputValue}/>
+        </div>
+      </div>
     </>
   )
 }
--- url-shortener-frontend/src/components/UrlContainer.jsx
+++ url-shortener-frontend/src/components/UrlContainer.jsx
@@ -1,26 +1,32 @@
-import { FaCopy } from "react-icons/fa";
+import { useState } from "react";
 
-const UrlContainer = () => {
+const UrlContainer = ({ setInputValue }) => {
+    const [urlLink, setUrlLink] = useState('')
+
+    const handleOnSubmit = () => {
+        setInputValue(urlLink);
+        setUrlLink('')
+    }
+
+
     return (
-        <div className='url-container'>
-            <div className='url-wrapper'>
-                <div className="url-form-container">
-                    <form autoComplete="off">
-                        <h3>URL Link Shortener</h3>
-                        <div className="form-group">
-                            <label>Enter URL link here</label>
-                            <div className='url-link-container'>
-                                <input type="url" className='form-control' />
-                                <button className='btn'>Get Link</button>
-                            </div>
-                        </div>
-                    </form>
+        <div className="url-form-container">
+            <form autoComplete="off">
+                <h3>URL Link Shortener</h3>
+                <div className="form-group">
+                    <label>Enter URL link here</label>
+                    <div className='url-link-container'>
+                        <input
+                            type="text"
+                            className='form-control'
+                            value={urlLink}
+                            onChange={(e) => setUrlLink(e.target.value)}
+                            required
+                        />
+                        <button onClick={handleOnSubmit} className='btn'>Get Link</button>
+                    </div>
                 </div>
-                <div className='url-shortLink-container'>
-                    <p className='result-text'>Link url</p>
-                    <FaCopy className="result-btn"/>
-                </div>
-            </div>
+            </form>
         </div>
     )
 }
