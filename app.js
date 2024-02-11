bf4067eb25cea37fa141b096cf3cee14ce43c735
xopherw <chriswwl95@gmail.com>
2024-02-11 22:52:58 +1100

    udpated POST and GET requests

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const bodyParser = require('body-parser');
+const Joi = require('joi');
 
 require('dotenv').config();
 
@@ -9,17 +10,40 @@ let url = process.env.DB_CONNECTION_DEV;
 const app = express();
 const client = new MongoClient(url);
 
-app.use(bodyParser.urlencoded({
-    extended: false
- }));
-
-// app.get('/url/lists', (req, res) => {
-
-// })
-
-app.post('/url/create/:input_url', async (req, res) => {
+// list out all the urls
+app.get('/url/list', async (req, res) => {
     try{
+        await client.connect();
+        const db = client.db("url_shortener");
+        const collection = db.collection("urls");
+        const data = await collection.find().toArray();
         
+        res.send(data);
+    }
+    catch(err){
+        res.status(500).send("Error getting documents");
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+})
+
+// create new url, possible shorten url logic
+app.post('/url/create/', async (req, res) => {
+    try{
+        const schema = {
+            input_url: Joi.string().uri().required().min(5).max(5000),
+            shorten_url: Joi.string().uri().required().min(5).max(5000)
+        }
+
+        const validate = Joi.validate(req.params, schema);
+
+        if(validate.error()){
+            res.status(400).send("Invalid url");
+            return;
+        }
+
         await client.connect();
         console.log("Connected correctly to server");
 
@@ -28,7 +52,8 @@ app.post('/url/create/:input_url', async (req, res) => {
         const collection = db.collection("urls");
 
         const doc = {
-            url: req.params.input_url
+            url: req.params.input_url,
+            shorten_url: req.params.shorten_url
         }
         const result = await collection.insertOne(doc);
         
@@ -36,6 +61,7 @@ app.post('/url/create/:input_url', async (req, res) => {
         res.status(200).send("Document inserted");
         
     } catch(err) {
+        res.status(500).send("Error creating document");
         console.log(err.stack);
     }
      finally {
@@ -44,14 +70,6 @@ app.post('/url/create/:input_url', async (req, res) => {
     }
 })
 
-app.put('/url/update/:url/:new_url', (req, res) => {
-    const params = req.params;
-    res.send(params)
-})
-
-app.delete('/url/remove/:url', (req, res) => {
-
-})
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listen on port ${port}`));
