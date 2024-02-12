9c90d315637fa9389947532c27956a4cfc69f213
unknown <fredileader@gmail.com>
2024-02-12 03:43:16 +0100

    work on form validation

--- url-shortener-frontend/src/components/UrlContainer.jsx
+++ url-shortener-frontend/src/components/UrlContainer.jsx
@@ -1,32 +1,51 @@
 import { useState } from "react";
 
 const UrlContainer = ({ setInputValue }) => {
-    const [urlLink, setUrlLink] = useState('')
+    const [urlLink, setUrlLink] = useState('');
+    const [errMsg, setErrMsg] = useState(false)
+
+    const REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
 
     const handleOnSubmit = () => {
-        setInputValue(urlLink);
+        // here is the form validation with REGEX
+        if (!urlLink) {
+            console.log('Please Insert your URL link')
+            setErrMsg('Please Insert your URL link')
+        } else if (!REGEX.test(urlLink)) {
+            console.log('Please input a valid URL link')
+            setErrMsg('Please input a valid URL link')
+        } else {
+            setErrMsg(false)
+            setInputValue(urlLink)
+        }
+
+        // To clear form after data has been sent
         setUrlLink('')
     }
 
 
     return (
         <div className="url-form-container">
-            <form autoComplete="off">
-                <h3>URL Link Shortener</h3>
-                <div className="form-group">
-                    <label>Enter URL link here</label>
-                    <div className='url-link-container'>
-                        <input
-                            type="text"
-                            className='form-control'
-                            value={urlLink}
-                            onChange={(e) => setUrlLink(e.target.value)}
-                            required
-                        />
-                        <button onClick={handleOnSubmit} className='btn'>Get Link</button>
-                    </div>
+            <h3>URL Link Shortener</h3>
+            <div className="form-group">
+                <label>Enter URL link here</label>
+                <div className='url-link-container'>
+                    <input
+                        type="text"
+                        className='form-control'
+                        value={urlLink}
+                        onChange={(e) => setUrlLink(e.target.value)}
+                        required
+                    />
+                    <button
+                        className='btn'
+                        onClick={handleOnSubmit}
+                    >
+                        Get Link
+                    </button>
                 </div>
-            </form>
+                <p className="wrong-input">{errMsg}</p>
+            </div>
         </div>
     )
 }
