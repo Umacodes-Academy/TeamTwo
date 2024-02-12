d43900f50ade99095da9b46ecef9e1a6f504595c
unknown <fredileader@gmail.com>
2024-02-12 03:39:47 +0100

    add loading for server delay and result messages from server

--- url-shortener-frontend/src/components/LinkResult.jsx
+++ url-shortener-frontend/src/components/LinkResult.jsx
@@ -1,22 +1,51 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FaCopy } from "react-icons/fa";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const LinkResult = ({ inputValue }) => {
-    console.log(inputValue)
-    const [shortenLink, setShortenLink] = useState('Generated Link')
+const LinkResult = ({ shortenLink, loading, error }) => {
+
     const [copied, setCopied] = useState(false)
 
+    
+    
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCopied(false);
+        }, 1000);
+        
+        return () => clearTimeout(timer)
+    }, [copied])
+    
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+    if (error) {
+        return <p>something went wrong with server</p>
+    }
+
     return (
-        <div className='url-shortLink-container'>
-            <p className='result-text'>{shortenLink}</p>
-            <CopyToClipboard 
-            text={shortenLink}
-            onCopy={(e) => setCopied(true)}
-            >
-                <FaCopy className="result-btn" />
-            </CopyToClipboard>
-        </div>
+        <>
+            {shortenLink &&
+                (
+                    <div className='url-shortLink-container'>
+                        {/* <p className='result-text'>{shortenLink}</p> */}
+                        <CopyToClipboard
+                            text={shortenLink}
+                            onCopy={() => setCopied(true)}
+                        >
+                            <FaCopy className="result-btn" />
+                        </CopyToClipboard>
+                        {copied &&
+                            (
+                                <div className='res'>
+                                    <span>Copied</span>
+                                </div>
+                            )
+                        }
+                    </div>
+                )}
+        </>
     )
 }
 
