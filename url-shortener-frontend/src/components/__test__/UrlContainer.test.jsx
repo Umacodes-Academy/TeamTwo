5a1df1fc04d724aff82fbff4d01c4842ea910826
unknown <fredileader@gmail.com>
2024-02-19 00:03:48 +0100

    add new folder for testing

---
+++ url-shortener-frontend/src/components/__test__/UrlContainer.test.jsx
@@ -0,0 +1,10 @@
+import UrlContainer from "../UrlContainer";
+
+
+// Test the urlContainer component
+describe('Test the url component', () => {
+  test('render the form with one button', () => {
+    render(<UrlContainer />);
+  })
+  
+})
