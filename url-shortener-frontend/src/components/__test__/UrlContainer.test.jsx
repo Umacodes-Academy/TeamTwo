7665d68aa3a856387f4aa9599ebdbdfeef7a75ea
unknown <fredileader@gmail.com>
2024-02-20 02:09:18 +0100

    create a folder and component for unit testing

--- url-shortener-frontend/src/components/__test__/UrlContainer.test.jsx
+++ url-shortener-frontend/src/components/__test__/UrlContainer.test.jsx
@@ -1,10 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
 import UrlContainer from "../UrlContainer";
 
 
 // Test the urlContainer component
-describe('Test the url component', () => {
-  test('render the form with one button', () => {
+describe('UrlContainer', () => {
+  it('URL link Shortener to be in the document', () => {
     render(<UrlContainer />);
-  })
-  
+    const urlContainerElement = screen.getByText('URL Link Shortener');
+    expect(urlContainerElement).toBeInTheDocument()
+  })  
 })
