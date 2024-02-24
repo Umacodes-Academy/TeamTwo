5ce5a42c57a380cd81542f5e22a7ee1a2e3c483c
unknown <fredileader@gmail.com>
2024-02-24 10:43:18 +0100

    create folder for vitest setup file

---
+++ url-shortener-frontend/tests/setup.js
@@ -0,0 +1,11 @@
+import { afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+
+// runs a cleanup after each test case
+afterEach(() => {
+  cleanup()
+})
+
+
+
