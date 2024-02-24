ad1b4de2bd743478d296ec3cfb38ee3ff6a7aa30
unknown <fredileader@gmail.com>
2024-02-24 10:41:17 +0100

    modify testing

--- url-shortener-frontend/src/components/__test__/UrlContainer.test.jsx
+++ url-shortener-frontend/src/components/__test__/UrlContainer.test.jsx
@@ -1,15 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import { describe, it, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import UrlContainer from "../UrlContainer";
 
 
 // Test the urlContainer component
 describe('UrlContainer', () => {
-  it('URL link Shortener to be in the document', () => {
+  test('Url Link Shortener text to be in the document', () => {
     render(<UrlContainer />);
-    const urlContainerElement = screen.getByText('URL Link Shortener');
+    const urlContainerElement = screen.getByText(/URL Link Shortener/i);
     expect(urlContainerElement).toBeInTheDocument()
-  })  
+  });
+
+  test('Url Link Shortener text to be in the document', () => {
+    render(<UrlContainer />);
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toHaveTextContent('Get Link')
+  })
 })
